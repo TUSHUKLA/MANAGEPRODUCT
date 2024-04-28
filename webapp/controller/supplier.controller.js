b9342900b62cf894b92e9526158b83fca72b3cdc
TUSHAR SHUKLA <tusharshukla0907@gmail.com>
2024-04-28 07:28:41 +0000

    supplier page binding

--- webapp/controller/supplier.controller.js
+++ webapp/controller/supplier.controller.js
@@ -1,16 +1,18 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/UIComponent"
+
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller,UIComponent) {
         "use strict";
 
         return Controller.extend("com.tushars.manageproduct.controller.supplier", {
             onInit: function () 
             {
-                this.getOwnerComponent().getRouter().getRoute("object").attachPatternMatched(this.onObjectMatched, this);
+                UIComponent.getRouterFor(this).getRoute("RouteViewSupplier").attachPatternMatched(this.onObjectMatched, this);
                 
                 
           
@@ -18,6 +20,9 @@ sap.ui.define([
             onObjectMatched : function (oEvent) {
                 var sObjectId =  oEvent.getParameter("arguments").pId;
                
+            },
+            getRouter : function () {
+                return UIComponent.getRouterFor(this);
             },
             
         });
