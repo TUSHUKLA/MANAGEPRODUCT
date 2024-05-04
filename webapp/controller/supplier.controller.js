b5e2c3cd47e6626293aad43f2008c52a88726ced
satya dwivedi <satyadwivedi1@gmail.com>
2024-05-04 11:50:32 +0000

    JSON binding supplier

--- webapp/controller/supplier.controller.js
+++ webapp/controller/supplier.controller.js
@@ -1,29 +1,58 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/core/UIComponent"
-
+    "sap/ui/core/UIComponent",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller,UIComponent) {
+    function (Controller,UIComponent, JSONModel) {
         "use strict";
 
         return Controller.extend("com.tushars.manageproduct.controller.supplier", {
-            onInit: function () 
-            {
+            onInit: function () {
                 UIComponent.getRouterFor(this).getRoute("RouteViewSupplier").attachPatternMatched(this.onObjectMatched, this);
                 
-                
-          
             },
+
             onObjectMatched : function (oEvent) {
-                var sObjectId =  oEvent.getParameter("arguments").pId;
-               
+                const that = this
+                var sPID =  oEvent.getParameter("arguments").pId;
+
+                const oModel = this.getOwnerComponent().getModel();
+                
+                // Read supplier data
+                oModel.read("/Products("+ sPID +")/Supplier", {
+                         success : function(oSupplierData, response) {
+                            console.log('supplier data = ', oSupplierData)
+                            let oJSONMOdel1 = new JSONModel()
+                            oJSONMOdel1.setData(oSupplierData)
+                            that.getView().setModel(oJSONMOdel1, "S")    
+                         },
+                         
+                         error: function(oError) {
+                            console.log(oError)
+                        }
+                })
+                //end
+
+                // Read product data
+                oModel.read("/Products("+sPID+")", {
+                    success : function(oSupplierData, response) {
+                       console.log('product data = ', oSupplierData)
+                       let oJSONMOdel1 = new JSONModel()
+                       oJSONMOdel1.setData(oSupplierData)
+                       that.getView().setModel(oJSONMOdel1, "PD")    
+                    },
+                    
+                    error: function(oError) {
+                       console.log(oError)
+                   }
+                 })    
             },
+
             getRouter : function () {
                 return UIComponent.getRouterFor(this);
-            },
-            
+            }
         });
     });
